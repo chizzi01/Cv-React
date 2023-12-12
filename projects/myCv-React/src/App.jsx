4098e722b519fb63996af7793a628a0b2cd57827
Agustin Chizzini Melo <aguscn2001@gmail.com>
2023-12-12 18:20:00 -0300

    first commit

---
+++ projects/myCv-React/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { SocialLinks } from './components/social-Links'
+
+export function App() {
+return (
+  <section className='App'>
+<SocialLinks></SocialLinks>
+
+  </section>
+
+)
+
+}
---
+++ projects/myCv-React/src/components/social-Links.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function SocialLinks() {
+    return (
+        <div className="social-links">
+            <h1>Hola mundo</h1>
+        </div>
+    );
+}
