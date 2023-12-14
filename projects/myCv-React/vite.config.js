8f4f26c9c32975fe660b56dc4ab9db9804982b15
Agustin Chizzini Melo <aguscn2001@gmail.com>
2023-12-14 12:25:42 -0300

    c

--- projects/myCv-React/vite.config.js
+++ projects/myCv-React/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://chizzi01.github.io/Cv-React/'
+  base: 'https://chizzi01.github.io/Cv-React'
 })
